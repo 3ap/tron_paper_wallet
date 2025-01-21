3416bc03cd0dc981def5cb3c1fcb2cb5d2d351c6
Sergey Nazaryev <sergey@nazaryev.com>
2025-01-22 01:21:33 +0200

    Add script to restore secret key from mnemonic words

---
+++ create_secret_key_from_mnemonic.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+from tronpy.keys import PrivateKey
+from tronpy.hdwallet import TRON_DEFAULT_PATH, key_from_seed, seed_from_mnemonic
+
+mnemonic = input("Please type your mnemonic words: ")
+seed = seed_from_mnemonic(mnemonic, passphrase="")
+key = key_from_seed(seed, TRON_DEFAULT_PATH)
+wallet = PrivateKey(key)
+
+with open("secret_key.txt", "w") as secret_key_file:
+    secret_key_file.write(f'{wallet}')
+
+print("Paper wallet recovered secret key saved.")
