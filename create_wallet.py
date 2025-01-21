376dfea698466210390185eb3e7043acc3a4c918
Sergey Nazaryev <sergey@nazaryev.com>
2025-01-22 01:21:02 +0200

    create_wallet: generate not only a private key but also mnemonic words to mnemonic.txt

--- create_wallet.py
+++ create_wallet.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python3
 from tronpy.keys import PrivateKey
+from tronpy.hdwallet import TRON_DEFAULT_PATH, generate_mnemonic, key_from_seed, seed_from_mnemonic
 
-wallet = PrivateKey.random()
+mnemonic = generate_mnemonic(12, "english")
+seed = seed_from_mnemonic(mnemonic, passphrase="")
+key = key_from_seed(seed, TRON_DEFAULT_PATH)
+wallet = PrivateKey(key)
 
 with open("secret_key.txt", "w") as secret_key_file:
     secret_key_file.write(f'{wallet}')
+with open("mnemonic.txt", "w") as mnemonic_file:
+    mnemonic_file.write(f'{mnemonic}')
 
 print("Paper wallet created and secret key saved.")
